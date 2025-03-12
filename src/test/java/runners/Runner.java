42b2fca12468236bb619b797f6fdc74e1c02171a
AlfredoMc95 <alfredomartcam@gmail.com>
2025-03-12 14:30:40 -0500

    change runner name

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,5 +13,5 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepDefinitions")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel, pretty, timeline:build/test-results/timeline")
-public class GetRunner {
+public class Runner {
 }
